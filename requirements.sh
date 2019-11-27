37f2d8c59318ca76f9666b5f0dc3713dea9ca6ae
Imperial Industries <33400784+Imperial-Industries@users.noreply.github.com>
2019-11-27 12:58:50 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Use only if you have a Linux system that does not have Python 3.6 installed already
+# Use only if you have a Ubuntu/Debian system that does not have Python 3.6 installed already
 sudo apt-get update
 sudo apt-get install python3.6
+sudo apt-get install python3-pip
